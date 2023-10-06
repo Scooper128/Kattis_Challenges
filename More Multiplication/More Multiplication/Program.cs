2e676a19567b74a04b14c462e8ed83c352794348
Rechlally <Sam@eastwind.karoo.co.uk>
2023-10-06 17:09:55 +0100

    more multiplaction

---
+++ More Multiplication/More Multiplication/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace More_Multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String num1=Console.ReadLine();
+            String num2=Console.ReadLine();
+            var num1List = num1.ToArray();
+            var num2List = num2.ToArray();
+            int[,] grid;
+            int[] gridTemp;
+            foreach(int app in num2List) {   
+                foreach (int num in num1List)
+                {
+                    gridTemp.Append(num * app);
+                }
+            }
+                                                            
+            
+        }
+    }
+}
