824118b38630daa7725fceb7e16bac88c7bb06e9
Sam Cooper <sam_cooper101@proton.me>
2023-10-06 21:29:45 +0100

    slight progress in Program.cs creation of GridValues.cs and addition of a plan file

---
+++ More Multiplication/More Multiplication/NumberGridValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Multiplication
+{
+    internal class NumberGridValues
+    {
+        public NumberGridValues(int[] list) 
+        {
+            int[] numList = list;
+        }
+    }
+}
