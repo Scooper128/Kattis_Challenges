383846a9f3a823c4ac01710b9104df1842ff542d
Rechlally <Sam@eastwind.karoo.co.uk>
2023-10-09 17:36:06 +0100

    started monoply challenge

---
+++ Monopoly/Monopoly/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Monopoly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hotels = int.Parse(Console.ReadLine());
+            String spaces = Console.ReadLine();
+            int[] spacesSplit = spaces.Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(spacesSplit);
+            
+        }
+    }
+}
